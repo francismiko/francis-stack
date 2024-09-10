f04dc6d645c4cd0557f45c0bee0a469939f97117
francismiko <1485059943@qq.com>
2024-09-10 12:21:10 +0800

    chore: configs

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
-import "./index.css";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+import "virtual:uno.css";
+import "@unocss/reset/sanitize/sanitize.css";
+import "@unocss/reset/sanitize/assets.css";
+
+const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import UnoCSS from "unocss/vite";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), TanStackRouterVite(), UnoCSS()],
 });
